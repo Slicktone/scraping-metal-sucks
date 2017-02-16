6f6b2eb15464e725083d4a2d18ffdb0f4a95a3e4
TonyUCLA <TonyUCLA>
2017-02-16 13:24:13 -0800

    Heroku Staging

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@
 $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
   }
 });
@@ -15,7 +14,7 @@ $(document).on("click", "p", function() {
   // Save the id from the p tag
   var thisId = $(this).attr("data-id");
 
-  // Now make an ajax call for the Article
+  // Ajax call for the Article
   $.ajax({
     method: "GET",
     url: "/articles/" + thisId
@@ -25,18 +24,13 @@ $(document).on("click", "p", function() {
       console.log(data);
       // The title of the article
       $("#comments").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
       $("#comments").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
       $("#comments").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#comments").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#comments").append("<button data-id='" + data._id + "' id='savecomment'>Comment!</button>");
 
       // If there's a comment in the article
       if (data.comments) {
-        // Place the title of the comment in the title input
         $("#titleinput").val(data.comments.title);
-        // Place the body of the comment in the body textarea
         $("#bodyinput").val(data.comments.body);
       }
     });
@@ -47,26 +41,22 @@ $(document).on("click", "#savecomment", function() {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
 
-  // Run a POST request to change the note, using what's entered in the inputs
+  // Run a POST request to change comments
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-      // Value taken from title input
       title: $("#titleinput").val(),
-      // Value taken from note textarea
       body: $("#bodyinput").val()
     }
   })
-    // With that done
+
     .done(function(data) {
-      // Log the response
       console.log(data);
       // Empty the comments section
       $("#comments").empty();
     });
 
-  // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var logger = require("morgan");
+var async = require("async");
 // Scrapers
 var request = require("request");
 var cheerio = require("cheerio");
 // Requiring Models
 var Comment = require("./models/Comment.js");
 var User = require("./models/User.js");
+var Articles = require("./models/Articles.js");
 // Bluebird promise
 var Promise = require("bluebird");
 
@@ -28,7 +30,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration
-mongoose.connect("mongodb://news-scraper");
+mongoose.connect("mongodb://localhost/metal-sucks-scraper");
 var db = mongoose.connection;
 
 // Error Handling
@@ -50,35 +52,58 @@ app.get("/", function(req, res) {
 // GET request to SCRAPE
 app.get("/scrape", function(req, res) {
     request("http://www.metalsucks.net", function(error, response, html) {
-        var $ = cherrio.load(html);
-        // Grabbing an element and running the function
-        $("article a").each(function(i, element) {
-            var result = {};
+        var $ = cheerio.load(html);
+        var scrapedArticle = $(".post-title");
 
-            result.title = $(this).children("a").text();
-            result.link = $(this).children("a").attr("href");
-            // Creating a new entry with Article model
-            var entry = new Article(result);
-            console.log(entry);
-            entry.save(function(error, doc) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log(doc);
-                    // For Async
-                    if (i === $("article h2").length - 1) {
-                        res.send("Scrape Complete");
-                    }
-                }
-            });
+    async.each(scrapedArticle, function(data, callback) {
+        var data = new Articles();
+        data.save(function(error) {
+            if(error) {
+                 return callback(error);
+            }
+            else {
+                callback(null);
+            }
         });
+    }, function(error) {
+        if(error) {
+            return console.log("Process stopped due to error");
+        } 
+        else {
+             console.log("Scraped data saved successfully");
+         }
+    });
+
+
+
+        // // Grabbing an element and running the function
+        // $(".post-title").each(function(i, element) {
+        //     var result = {};
+
+        //     result.title = $(this).children("a").text();
+        //     result.link = $(this).children("a").attr("href");
+        //     // Creating a new entry with Article model
+        //     var entry = new Articles(result);
+        //     console.log(entry);
+        //     entry.save(function(error, doc) {
+        //         if (error) {
+        //             console.log(error);
+        //         }
+        //         else {
+        //             console.log(doc);
+        //             // For Async
+        //             if (i === $(".post-title").length - 1) {
+        //                 res.send("Scrape from www.metalsucks.net complete!");
+        //             }
+        //         }
+        //     });
+        // });
     });
 });
 
 // Grabbing ALL the articles
 app.get("/articles", function(req, res) {
-    Article.find({}, function(error, doc) {
+    Articles.find({}, function(error, doc) {
         if(error) {
             console.log(error);
         }
@@ -90,7 +115,7 @@ app.get("/articles", function(req, res) {
 
 // Grabbing the articles by ID
 app.get("/articles/:id", function(req, res) {
-    Article.findOne({"_id": req.params.id})
+    Articles.findOne({"_id": req.params.id})
     .populate("comment")
     .exec(function(error, doc){
         if(error) {
@@ -113,11 +138,8 @@ app.post("/articles/:id", function(req, res) {
             else {
                 // =========== TESTING THE COMMENT ===========
                 console.log(comment);
-                {
-                    _id: "12345",
-                    text: "****TEST***"
-                }
-                Article.findById(req.params.id, function(error, article) {
+
+                Articles.findById(req.params.id, function(error, article) {
                     if(error) {
                         return console.log(error);
                     }
@@ -139,5 +161,5 @@ app.post("/articles/:id", function(req, res) {
 
 
 app.listen(3000, function() {
-    console.log("This App is working...? Port 3000 I think");
+    console.log("If you are seeing this, THIS PORT IS ON 3000");
 });
